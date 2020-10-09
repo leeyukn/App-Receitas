069a31ecdc5bab8edbcdd126215e5d191e2b30e1
leeyukn <lee.yu20@protonmail.com>
2020-10-09 23:50:19 +0900

    Upload files version 1

---
+++ lib/screens/favorite_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../components/meal_item.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+
+  const FavoriteScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('Nenhuma refeição marcada como favorito!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(favoriteMeals[index]);
+        },
+      );
+    }
+  }
+}
